e674b19348c0a5d568bbc5559d2ca5eddb04a658
Basics <florenceobirimainoo@gmail.com>
2023-05-03 11:22:13 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - Writes the character c
+ * @c: The character to print
+ * Return: Success 1.
+ * Error, -1 is returned, and the error is set approximately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
